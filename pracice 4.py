fc6371ac05bbf09f3a8d0282f6543be8ca124339
YemRuthie <49885158+YemRuthie@users.noreply.github.com>
2019-05-21 15:25:58 +0100

    Update pracice 4.py

--- pracice 4.py
+++ pracice 4.py
@@ -1,10 +1,8 @@
 #practice 4
+# edited
 
 x = range(2, 11)
 
 num= int(input("Enter num: "))
 
-for i in x:
-    if ( num % i == 0):
-         print (i)
-     
+print("New List 3",[i for i in x if num % i == 0])
